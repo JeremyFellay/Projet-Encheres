74f516a1696923c96c942d3fbf4bc6603fb86705
Jérémy Fellay <jeremy.fellay@hotmail.fr>
2023-12-16 14:15:50 +0100

    Rajout class Enchere couche BO

---
+++ ProjetEncheres/src/main/java/fr/eni/encheres/bo/Enchere.java
@@ -0,0 +1,11 @@
+package fr.eni.encheres.bo;
+
+import java.time.LocalDate;
+
+public class Enchere {
+	
+	private LocalDate dateEnchere;
+	private int montantEnchere;
+	private Utilisateur utilisateur;
+	
+}
